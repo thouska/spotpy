8bb0ef0b65e659243f0fe79375b474ab0689f2cb
thouska <tobias.houska@umwelt.uni-giessen.de>
2017-02-02 15:03:24 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name = 'spotpy',
-  version = '1.2.29',
+  version = '1.2.30',
   description = 'A Statistical Parameter Optimization Tool',
 
   author = 'Tobias Houska, Philipp Kraft, Alejandro Chamorro-Chavez and Lutz Breuer',
