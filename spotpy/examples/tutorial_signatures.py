d98ebf4f5b36e124b7b634cb9c2ba8f0bed8fcbe
thouska <tobias.houska@umwelt.uni-giessen.de>
2017-05-22 14:07:20 +0200

    Clean up

--- spotpy/examples/tutorial_signatures.py
+++ spotpy/examples/tutorial_signatures.py
@@ -3,23 +3,8 @@
 Copyright (c) 2017 by Benjamin Manns
 This file is part of Statistical Parameter Estimation Tool (SPOTPY).
 :author: Benjamin Manns
-:paper: Houska, T., Kraft, P., Chamorro-Chavez, A. and Breuer, L.: 
-SPOTting Model Parameters Using a Ready-Made Python Package, 
-PLoS ONE, 10(12), e0145180, doi:10.1371/journal.pone.0145180, 2015.
-This package enables the comprehensive use of different Bayesian and Heuristic calibration 
-techniques in one Framework. It comes along with an algorithms folder for the 
-sampling and an analyser class for the plotting of results by the sampling.
-:dependencies: - Numpy >1.8 (http://www.numpy.org/) 
-               - Pandas >0.13 (optional) (http://pandas.pydata.org/)
-               - Matplotlib >1.4 (optional) (http://matplotlib.org/) 
-               - CMF (optional) (http://fb09-pasig.umwelt.uni-giessen.de:8000/)
-               - mpi4py (optional) (http://mpi4py.scipy.org/)
-               - pathos (optional) (https://pypi.python.org/pypi/pathos/)
-               :help: For specific questions, try to use the documentation website at:
-http://fb09-pasig.umwelt.uni-giessen.de/spotpy/
-For general things about parameter optimization techniques have a look at:
-https://github.com/CamDavidsonPilon/Probabilistic-Programming-and-Bayesian-Methods-for-Hackers/tree/master/
-Pleas cite our paper, if you are using SPOTPY.
+
+This code shows you, how to use the hydroligcal signatures. They can also be implemented in the def objective function.
 '''
 
 from __future__ import absolute_import
