790881fa3e84ec26f0d94dcd1e0c770e6ddc6ebc
thouska <tobias.houska@umwelt.uni-giessen.de>
2017-05-31 10:22:51 +0200

    Update demcz.py

--- spotpy/algorithms/demcz.py
+++ spotpy/algorithms/demcz.py
@@ -52,7 +52,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from ._algorithm import _algorithm
+from . import _algorithm
 import numpy as np
 import time
 
