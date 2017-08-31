1515b1e17caf21924b6dceb4fc15a85ace5b16af
Florian Jehn <uaw80@gmx.de>
2017-08-31 10:07:23 +0200

    Update rope.py

--- spotpy/algorithms/rope.py
+++ spotpy/algorithms/rope.py
@@ -183,8 +183,8 @@ class rope(_algorithm):
         if repetitions_following_runs is not None:
             runs = repetitions_following_runs
 
-        for i in range(subsets - 1):
-            if subsets == 0:
+        for subset in range(subsets - 1):
+            if subset == 0:
                 best_pars = self.get_best_runs(likes, pars, runs, 
                                                percentage_first_run)
             else:
