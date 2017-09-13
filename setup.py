406cb22a8a2e7c94278b08162912ff64cd89655b
thouska <tobias.houska@umwelt.uni-giessen.de>
2017-09-13 10:14:57 +0200

    Include hymod example for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
   name = 'spotpy',
-  version = '1.3.6',
+  version = '1.3.7',
   description = 'A Statistical Parameter Optimization Tool',
   long_description=open(os.path.join(os.path.dirname(__file__),
                                        "README.rst")).read(),
@@ -14,7 +14,10 @@ setup(
   url = 'http://www.uni-giessen.de/cms/faculties/f09/institutes/ilr/hydro/download/spotpy',
   license = 'MIT',
   packages = ["spotpy", "spotpy.examples", "spotpy.examples.hymod", "spotpy.algorithms", "spotpy.parallel"],
-  include_package_data = True,
+  package_data={
+   'spotpy.examples.hymod': ['*'],
+   },
+  #include_package_data = True,
   use_2to3 = True,
   keywords = ['Monte Carlo', 'MCMC','MLE', 'SCE-UA', 'Simulated Annealing', 'DE-MCz', 'DREAM', 'ROPE', 'Artifical Bee Colony', 'Uncertainty', 'Calibration', 'Model', 'Signatures'],
   classifiers = [
