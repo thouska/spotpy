13e4e0ae801a6635ca87887d3dd1568125c9707e
Florian Jehn <florian.u.jehn@umwelt.uni-giessen.de>
2017-12-12 12:54:37 +0100

    Update abc.py

--- spotpy/algorithms/abc.py
+++ spotpy/algorithms/abc.py
@@ -58,7 +58,7 @@ class abc(_algorithm):
         *False: Simulationt results will not be saved
      '''
 
-    def __init__(self, spot_setup, dbname=None, dbformat=None, parallel='seq', save_sim=True):
+    def __init__(self, spot_setup, dbname=None, dbformat=None, parallel='seq', save_sim=True, save_threshold=-np.inf):
 
         _algorithm.__init__(self, spot_setup, dbname=dbname,
                             dbformat=dbformat, parallel=parallel, save_sim=save_sim,
