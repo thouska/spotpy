2f9b113c62c0071eb35a0945ee3c48f56dfd80c5
Florian Jehn <florian.u.jehn@umwelt.uni-giessen.de>
2017-12-12 12:55:55 +0100

    Update sa.py

--- spotpy/algorithms/sa.py
+++ spotpy/algorithms/sa.py
@@ -50,7 +50,7 @@ class sa(_algorithm):
         *False: Simulationt results will not be saved
      '''
 
-    def __init__(self, spot_setup, dbname=None, dbformat=None, parallel='seq', save_sim=True):
+    def __init__(self, spot_setup, dbname=None, dbformat=None, parallel='seq', save_sim=True, save_threshold=-np.inf):
 
         _algorithm.__init__(self, spot_setup, dbname=dbname,
                             dbformat=dbformat, parallel=parallel, save_sim=save_sim,
