97e3309a66c5d84489df4a384552e5b5d75643ea
Havan <havanagrawal@gmail.com>
2018-01-25 01:55:55 -0800

    Add tests for pbias and nashsutcliffe

--- spotpy/unittests/test_objectivefunctions.py
+++ spotpy/unittests/test_objectivefunctions.py
@@ -14,19 +14,24 @@ class TestObjectiveFunctions(unittest.TestCase):
         self.simulation = np.random.randn(10)
         self.evaluation = np.random.randn(10)
 
-        print(self.simulation)
-        print(self.evaluation)
-
     def test_bias(self):
         res = of.bias(self.evaluation, self.simulation)
         self.assertAlmostEqual(res, 1.2387193462811703, self.tolerance)
+
+    def test_pbias(self):
+        res = of.pbias(self.evaluation, self.simulation)
+        self.assertAlmostEqual(res, -156.66937901878677, self.tolerance)
+
+    def test_nashsutcliffe(self):
+        res = of.nashsutcliffe(self.evaluation, self.simulation)
+        self.assertAlmostEqual(res, -4.1162070769985508, self.tolerance)
 
     def test_length_mismatch_return_nan(self):
         all_funcs = of._all_functions
 
         for func in all_funcs:
             res = func([0], [0, 1])
-            self.assertIs(res, np.nan, "Expected np.nan in length mismatch, Got {}".format(res))
+            self.assertTrue(np.isnan(res), "Expected np.nan in length mismatch, Got {}".format(res))
 
 
 if __name__ == '__main__':
