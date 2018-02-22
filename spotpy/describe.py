8d682bf6013249b143c87e7c9533cde092eeb4ae
kraft-p <oekopez@gmx.de>
2018-02-22 14:33:35 +0100

    Using rst to describe setups and samplers

--- spotpy/describe.py
+++ spotpy/describe.py
@@ -14,6 +14,7 @@ Usage:
 from __future__ import division, absolute_import, unicode_literals
 import sys
 from .parameter import get_parameters_from_setup
+from .algorithms._algorithm import _algorithm
 if sys.version_info[0] >= 3:
     from inspect import getdoc as _getdoc
     unicode = str
@@ -27,6 +28,12 @@ else:
                 '{}: Docstring uses unicode but {} misses the line ``from __future__ import unicode_literals``'
                 .format(obj, type(obj).__module__)
                 )
+
+try:
+    from docutils.core import publish_string
+except ImportError:
+    publish_string = None
+
 
 def describe(obj):
     """
@@ -67,3 +74,72 @@ def setup(obj):
     params = '\n'.join(' - {p}'.format(p=unicode(p)) for p in get_parameters_from_setup(obj))
     parts = [cname, '=' * len(cname), mdoc, 'Parameters:', '-' * 11, params]
     return '\n'.join(parts)
+
+
+def _as_rst_caption(s, caption_sign):
+    s = unicode(s)
+    return s + '\n' + caption_sign * len(s) + '\n\n'
+
+class rst:
+    """
+    Creates a reStructuredText description of a sampler or a setup
+    """
+    def __init__(self, setup_or_sampler, extra_rst=''):
+        if isinstance(setup_or_sampler, _algorithm):
+            self.setup = setup_or_sampler.setup
+            self.sampler = setup_or_sampler
+            self.rst_text = self._sampler_text()
+        else:
+            self.setup = setup_or_sampler
+            self.sampler = None
+            self.rst_text = ''
+
+        self.rst_text += self._setup_text()
+
+    def __str__(self):
+        if sys.version_info.major < 3:
+            return self.rst_text.encode('utf-8', errors='ignore')
+        else:
+            return self.rst_text
+
+    def __unicode__(self):
+        return self.rst_text
+
+    def as_html(self, stylesheet=None):
+        """
+        Converts the generated reStructuredText as html5
+        :return: The html document as string
+        """
+        if publish_string is None:
+            raise NotImplementedError('The docutils package needs to be installed')
+        args = {'stylesheet_path': stylesheet or 'describe.css',
+                'input_encoding': 'unicode',
+                'output_encoding' : 'unicode'}
+        res = publish_string(source=self.rst_text,
+                                           writer_name='html5',
+                                           settings_overrides=args)
+        return res
+
+    def _sampler_text(self):
+        obj = self.sampler
+        cname = _as_rst_caption(type(obj).__name__, '=')
+        s = [
+             '- **db format:** ' + obj.dbformat,
+             '- **db name:** ' + obj.dbname,
+             '- **save simulation:** ' + str(obj.save_sim),
+             '- **parallel:** ' + type(obj.repeat).__module__.split('.')[-1],
+             '', ''
+             ]
+        return cname + _getdoc(obj).strip('\n') + '\n\n' + '\n'.join(s)
+
+    def _setup_text(self):
+        # Get class name
+        obj = self.setup
+        cname = _as_rst_caption(type(obj).__name__, '=')
+        # Add doc string
+        mdoc = _getdoc(obj).strip('\n').replace('\r', '\n') + '\n\n'
+        # Get parameters from class
+        param_caption = _as_rst_caption('Parameters', '-')
+        params = '\n'.join('#. **{p.name}:** {p}'.format(p=p) for p in get_parameters_from_setup(obj))
+        return cname + mdoc + param_caption + params
+
--- spotpy/unittests/test_describe.py
+++ spotpy/unittests/test_describe.py
@@ -43,7 +43,9 @@ class TestDescribe(unittest.TestCase):
         io.write(t)
         self.assertGreaterEqual(io.getvalue().count('\n'), 6)
 
-    def sampler_test(self, sampler):
+    def sampler_test(self, sampler=None):
+        if not sampler:
+            return
         t = spotpy.describe.describe(sampler)
         io = StringIO()
         io.write(t)
@@ -68,7 +70,21 @@ class TestDescribe(unittest.TestCase):
                 self.sampler_test(sampler)
 
 
+class TestDescribeRst(unittest.TestCase):
+
+    def test_setup_rst(self):
+        setup = SpotSetup()
+        rst = spotpy.describe.rst(setup)
+
+
+    def test_sampler_rst(self):
+        for sname, scls in inspect.getmembers(spotpy.algorithms, inspect.isclass):
+            if not sname.startswith('_'):
+                print(sname)
+                model = SpotSetup()
+                sampler = scls(spot_setup=model, dbformat='ram', dbname='äöü')
+                rst = spotpy.describe.rst(sampler)
+                rst.as_html()
 
 if __name__ == '__main__':
-
     unittest.main()
