c8a5d68110f6c064027867611677168259b118e3
kraft-p <oekopez@gmx.de>
2018-02-22 13:21:35 +0100

    Added module variables for #120

--- spotpy/parallel/mproc.py
+++ spotpy/parallel/mproc.py
@@ -12,12 +12,14 @@ from __future__ import unicode_literals
 
 import pathos.multiprocessing as mp
 
+process_count = None
+
 class PhaseChange(object):
     """
     Object to identify a change of a simulation phase
     """
-    def __init__(self,phase):
-        self.phase=phase
+    def __init__(self, phase):
+        self.phase = phase
         
 class ForEach(object):
     """
@@ -25,29 +27,29 @@ class ForEach(object):
     We using the pathos multiprocessing module and the orderd map function where results are saved until results in
     the given order are caluculated. We yielding back the result so a generator object is created.
     """
-    def __init__(self,process):
-        self.size = mp.cpu_count()
+    def __init__(self, process):
+        self.size = process_count or mp.cpu_count()
         self.process = process
         self.phase=None
         self.pool = mp.ProcessingPool(self.size)
 
     def is_idle(self):
         return False
+
     def terminate(self):
         pass
 
     def start(self):
         pass
 
-    def setphase(self,phasename):
-        self.phase=phasename
-
+    def setphase(self, phasename):
+        self.phase = phasename
 
     def f(self, job):
         data = self.process(job)
         return data
 
-    def __call__(self,jobs):
+    def __call__(self, jobs):
         results = self.pool.imap(self.f, jobs)
         for i in results:
             yield i
