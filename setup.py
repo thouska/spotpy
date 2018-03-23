6acd686abc4a602930433aa559e5f3cefd595822
thouska <tobias.houska@umwelt.uni-giessen.de>
2018-03-23 10:38:39 +0100

    Updated version number and a missing bracket #147

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
   name = 'spotpy',
-  version = '1.3.26',
+  version = '1.3.27',
   description = 'A Statistical Parameter Optimization Tool',
   long_description=open(os.path.join(os.path.dirname(__file__),
                                        "README.rst")).read(),
