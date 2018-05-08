045b637793773049730c9cf85b5a284c219ea60d
thouska <tobias.houska@umwelt.uni-giessen.de>
2018-05-08 12:57:43 +0200

    Removed use of a threshold in FAST plot

--- spotpy/examples/tutorial_fast_hymod.py
+++ spotpy/examples/tutorial_fast_hymod.py
@@ -9,8 +9,8 @@ This class holds example code how to use the dream algorithm
 '''
 
 import spotpy
-#from spotpy.examples.spot_setup_hymod_exe import spot_setup
-from spotpy.examples.spot_setup_hymod_python import spot_setup
+from spotpy.examples.spot_setup_hymod_exe import spot_setup
+#from spotpy.examples.spot_setup_hymod_python import spot_setup
 
 
 if __name__ == "__main__":
@@ -19,16 +19,16 @@ if __name__ == "__main__":
     spot_setup=spot_setup()
     
     #Select number of maximum repetitions
-    rep=5000
+    rep=200
     
     #Start a sensitivity analysis
     sampler=spotpy.algorithms.fast(spot_setup, dbname='FAST_hymod', dbformat='csv')
-    sampler.sample(rep)
+    #sampler.sample(rep)
     
     # Load the results gained with the dream sampler, stored in DREAM_hymod.csv
     results = spotpy.analyser.load_csv_results('FAST_hymod')
     
     # Example plot to show the sensitivity index of each parameter
-    spotpy.analyser.plot_fast_sensitivity(results)
+    spotpy.analyser.plot_fast_sensitivity(results, number_of_sensitiv_pars=3)
 
     
