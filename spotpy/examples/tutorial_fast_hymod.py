dd3e91c6620df15bb00ff3699451f95903a212f2
thouska <tobias.houska@umwelt.uni-giessen.de>
2018-07-24 16:12:42 +0200

    Updated documentation

--- spotpy/examples/tutorial_fast_hymod.py
+++ spotpy/examples/tutorial_fast_hymod.py
@@ -16,19 +16,20 @@ from spotpy.examples.spot_setup_hymod_exe import spot_setup
 if __name__ == "__main__":
     parallel ='seq'
     # Initialize the Hymod example
-    spot_setup=spot_setup()
+    spot_setup = spot_setup()
     
     #Select number of maximum repetitions
-    rep=1000
+    rep = 1000
     
     #Start a sensitivity analysis
-    sampler=spotpy.algorithms.fast(spot_setup, dbname='FAST_hymod', dbformat='csv')
-    #sampler.sample(rep)
+    sampler = spotpy.algorithms.fast(spot_setup, dbname='FAST_hymod', dbformat='csv')
+    sampler.sample(rep)
     
-    # Load the results gained with the dream sampler, stored in DREAM_hymod.csv
+    # Load the results gained with the fast sampler, stored in FAST_hymod.csv
     results = spotpy.analyser.load_csv_results('FAST_hymod')
     
     # Example plot to show the sensitivity index of each parameter
     spotpy.analyser.plot_fast_sensitivity(results, number_of_sensitiv_pars=3)
 
-    
+     # Example to get the sensitivity index of each parameter
+     SI = spotpy.analyser.get_sensitivity_of_fast(results)  
