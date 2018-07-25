520e21b6fd8a7b4678dec6dc289a7d2d02edfa40
thouska <tobias.houska@umwelt.uni-giessen.de>
2018-07-25 14:40:00 +0200

    Fix for fast test

--- spotpy/unittests/test_fast.py
+++ spotpy/unittests/test_fast.py
@@ -28,7 +28,7 @@ class TestFast(unittest.TestCase):
 
         sampler.sample(self.rep)
         results = sampler.getdata()
-        self.assertEqual(805,len(results))
+        self.assertEqual(800,len(results))
 
 
 
