aeff6cfd2c8995b57bd56f128a37516c821b4ca4
thouska <tobias.houska@umwelt.uni-giessen.de>
2018-07-25 15:21:12 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
   name = 'spotpy',
-  version = '1.3.27',
+  version = '1.3.28',
   description = 'A Statistical Parameter Optimization Tool',
   long_description=open(os.path.join(os.path.dirname(__file__),
                                        "README.rst")).read(),
@@ -13,7 +13,8 @@ setup(
   author_email = 'tobias.houska@umwelt.uni-giessen.de',
   url = 'http://www.uni-giessen.de/cms/faculties/f09/institutes/ilr/hydro/download/spotpy',
   license = 'MIT',
-  packages = ["spotpy", "spotpy.examples", "spotpy.examples.hymod_python", "spotpy.examples.hymod_exe", "spotpy.algorithms", "spotpy.parallel", "spotpy.gui", "spotpy.hydrology"],
+  packages = ["spotpy", "spotpy.examples", "spotpy.examples.hymod_python", "spotpy.examples.hymod_exe", 
+  "spotpy.examples.cmf_data", "spotpy.algorithms", "spotpy.parallel", "spotpy.gui", "spotpy.hydrology"],
   package_data={
    'spotpy.examples.hymod_exe': ['*'],
    'spotpy.examples.hymod_python': ['*'],
@@ -30,7 +31,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules'],
