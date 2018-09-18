685caf0710de1817af23463652282db17d517d6c
bees4ever <beesforever@gmx.de>
2018-09-18 21:47:33 +0200

    implement distinct parameters sampling in DDS with spotpy's parameter module

--- spotpy/examples/tutorial_dds.py
+++ spotpy/examples/tutorial_dds.py
@@ -10,11 +10,11 @@ except ImportError:
     sys.path.append(".")
     import spotpy
 
-import numpy as np
+
 from spotpy.describe import describe
 
 from spotpy.examples.spot_setup_dds import spot_setup
-
+from spotpy.examples.spot_setup_dds import ackley10
 
 #Create samplers for every algorithm:
 results=[]
@@ -27,15 +27,21 @@ timeout=10 #Given in Seconds
 Initial_solution = [] # TODO if user had seom, read it in
 
 
+
+
 parallel = "seq"
 dbformat = "csv"
 
 sampler=spotpy.algorithms.DDS(spot_setup,parallel=parallel, dbname='DDS', dbformat=dbformat, sim_timeout=timeout)
-#print(describe(sampler))
-sampler.sample(rep)
-#results.append(sampler.getdata())
+
+print(describe(sampler))
+sampler.sample(rep,fraction1=0.2,trials=2)
+
+#print(sampler.getdata())
+
+results.append(sampler.getdata())
 
 
 
-print(results[0].dtype) # Check for Travis: Get the last sampled parameter for x
+#print(results[0].dtype) # Check for Travis: Get the last sampled parameter for x
 evaluation = spot_setup.evaluation()
