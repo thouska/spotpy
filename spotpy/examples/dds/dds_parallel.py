fe4d8812bbebb9caee820a1c81ee7cfc4ee9adf2
Allan Karlson <beesforever@gmx.de>
2019-01-25 08:53:33 +0100

    DDS parallel tests and results

--- spotpy/examples/dds/dds_parallel.py
+++ spotpy/examples/dds/dds_parallel.py
@@ -2,11 +2,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from pprint import pprint
 import numpy as np
+import sys
+import os
 import matplotlib.pylab as plt
 import json
-
 import time
 
 try:
@@ -19,65 +19,28 @@ except ImportError:
 
 from spotpy.examples.spot_setup_hymod_python import spot_setup
 
-
 spot_setup = spot_setup()
 
-# Create samplers for every algorithm:
-results = []
-benchmarks_duration = {"dds":[], "sceua":[], "dds_like":[],"sceua_like":[]}
-reps = [300, 1000, 3000, 4000, 5000, 10000]
+
+path = os.path.abspath(os.path.dirname(__file__))
+json_path = path + "/dds_parallel_data.json"
+benchmarks_duration = json.load(open(json_path))
 
 
-for rep in reps:
+rep = int(sys.argv[1])
+timeout = 10  # Given in Seconds
+parallel = "mpi"
+dbformat = "csv"
+start = time.time()
+dds_sampler = spotpy.algorithms.DDS(spot_setup, parallel=parallel, dbname='DDS', dbformat=dbformat, sim_timeout=timeout)
+dds_sampler.sample(rep, trials=1)
+dds_elapsed = time.time() - start
+print(dds_elapsed)
 
-    timeout = 10  # Given in Seconds
+benchmarks_duration["dds_duration"].append(dds_elapsed)
+benchmarks_duration["dds_like"].append(dds_sampler.status.objectivefunction)
+benchmarks_duration["rep"].append(rep)
 
-    parallel = "mpc"
-    dbformat = "csv"
+print(benchmarks_duration)
 
-    start = time.time()
-    dds_sampler = spotpy.algorithms.DDS(spot_setup, parallel=parallel, dbname='DDS', dbformat=dbformat, sim_timeout=timeout)
-    dds_sampler.sample(rep, trials=1)
-    results.append(dds_sampler.getdata())
-    dds_elapsed = time.time() - start
-
-    benchmarks_duration["dds"].append(dds_elapsed)
-    benchmarks_duration["dds_like"].append(dds_sampler.status.objectivefunction)
-
-
-
-
-def autolabel(rects):
-    """
-    Attach a text label above each bar displaying its height
-    """
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
-                '%d' % int(height),
-                ha='center', va='bottom')
-
-
-
-
-
-fig = plt.figure(figsize=(10, 6))
-ax = plt.subplot(111)
-rep_labels = [str(j) for j in reps]
-x_pos = [i for i, _ in enumerate(rep_labels)]
-X = np.arange(len(benchmarks_duration["dds"]))
-dds_time = ax.bar(x_pos, benchmarks_duration["dds"], color = 'b', width = 0.45)
-
-plt.xticks(x_pos, rep_labels)
-plt.xlabel("Repetitions")
-plt.ylabel("Best Objective Function Value")
-
-autolabel(dds_plot)
-autolabel(sceua_plot)
-
-plt.savefig("MPI_TEST")
-
-
-dds_like = ax.bar([j+0.45 for j in x_pos], benchmarks_duration["dds_like"], color = 'g', width = 0.45)
-
-plt.ylabel("Best Objective Function Value")
+json.dump(benchmarks_duration, open(json_path,"w"))
