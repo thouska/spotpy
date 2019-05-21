13469757db7929c04b8420bc92ed523fb651969f
thouska <tobias.houska@umwelt.uni-giessen.de>
2019-05-21 10:24:36 +0200

    Update version number

--- spotpy/__init__.py
+++ spotpy/__init__.py
@@ -39,4 +39,4 @@ from . import examples            # Contains tutorials how to use SPOTPY
 from . import describe            # Contains some helper functions to describe smaplers and setups
 from .hydrology import signatures # Quantifies goodness of fit between simulation and evaluation data with hydrological signatures
 
-__version__ = '1.5.0'
+__version__ = '1.5.1'
