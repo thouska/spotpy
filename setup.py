269a5a7435f1e45d7ad90bb32d4ed9df89f77943
thouska <tobias.houska@umwelt.uni-giessen.de>
2019-07-08 12:57:49 +0200

    Update version number #223

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
   name = 'spotpy',
-  version = '1.5.1',
+  version = '1.5.2',
   description = 'A Statistical Parameter Optimization Tool',
   long_description=open(os.path.join(os.path.dirname(__file__),
                                        "README.rst")).read(),
