d175060603879e1e058e6c9c173c35d1061dca5a
Chris <chris_pw@gmx.de>
2021-05-28 11:12:21 +0200

    Adds logdir option and click parameter delegation for _algorithm
    * Also new logging instantiation

--- spotpy/algorithms/_algorithm.py
+++ spotpy/algorithms/_algorithm.py
@@ -212,10 +212,11 @@ class _algorithm(object):
     def __init__(self, spot_setup, dbname=None, dbformat=None, dbinit=True,
                  dbappend=False, parallel='seq', save_sim=True, breakpoint=None,
                  backup_every_rep=100, save_threshold=-np.inf, db_precision=np.float16,
-                 sim_timeout=None, random_state=None, optimization_direction='grid', algorithm_name=''):
+                 sim_timeout=None, random_state=None, optimization_direction='grid', algorithm_name='',
+                 quiet=False, logfile=None, logdir=None):
 
         # Instatiate logging
-        self.logger = spotpylogging.get_logger(self.__class__.__name__)
+        self.logger = spotpylogging.instantiate_logger(self.__class__.__name__, quiet, logfile, logdir)
 
         # Initialize the user defined setup class
         self.setup = spot_setup
--- spotpy/spotpylogging.py
+++ spotpy/spotpylogging.py
@@ -7,34 +7,63 @@ Defines a standard logger for the main application, which every child can
 
 import logging
 from datetime import datetime
+from os import path
 
-path_to_logfile = '{}-spotpy.log'.format(datetime.isoformat(datetime.now()))
+path_to_logfile = None
 
 # Standard defintion of main logger
 spotpy_logger = logging.getLogger("spotpy")
-if not spotpy_logger.handlers:
-    # create the handlers and call logger.addHandler(logging_handler)
-    # Add logging to stdout
-    handler_stdout = logging.StreamHandler()
-    formatter_file = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s ' +
-                                  '%(message)s')
-    formatter_stdout = logging.Formatter('%(message)s')
 
-    handler_stdout.setFormatter(formatter_stdout)
-    spotpy_logger.addHandler(handler_stdout)
-    
-    # Add logging to file
-    handler_file = logging.FileHandler(path_to_logfile)  # TODO this should be bound to cli arguments
-    handler_file.setFormatter(formatter_file)
-    spotpy_logger.addHandler(handler_file)
+handler_stdout = None
+handler_file = None
 
-    spotpy_logger.setLevel(logging.INFO)  # TODO this should be bound to cli arguments    
-    spotpy_logger.info('Write logging output to file \'%s\'', path_to_logfile)
-    
+formatter_file = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+formatter_stdout = logging.Formatter('%(message)s')
+
+def instantiate_logger(name, quiet=None, logfile=None, logdir=None):
+
+
+    path_to_logdir = '.'
+    path_to_logfile = '{}-spotpy.log'.format(datetime.isoformat(datetime.now()))
+
+    print(quiet, logfile, logdir)
+
+    if not spotpy_logger.handlers:
+      # create the handlers and call logger.addHandler(logging_handler)
+      # Add logging to stdout
+      handler_stdout = logging.StreamHandler()
+      
+      handler_stdout.setFormatter(formatter_stdout)
+      spotpy_logger.addHandler(handler_stdout)
+
+      if quiet:
+        handler_stdout.setLevel(logging.ERROR)
+
+      # Add logging to file
+      if logdir is not None:
+        path_to_logdir = logdir
+      if logfile is not None:
+        path_to_logfile = logfile
+      path_to_logfile = path_to_logdir + path.sep + path_to_logfile
+
+      handler_file = logging.FileHandler(path_to_logfile)  # TODO this should be bound to cli arguments
+      handler_file.setFormatter(formatter_file)
+      spotpy_logger.addHandler(handler_file)
+
+      spotpy_logger.setLevel(logging.INFO)  # TODO this should be bound to cli arguments
+      spotpy_logger.info('Write logging output to file \'%s\'', path_to_logfile)
+
+      return get_logger(name)
+    #else:
+      #if quiet and not (logfile is None):
+      #  raise RuntimeError("Logger is already instantiated!")
+
+    # Set the logger name, with the implementation class
 
 def get_logger(name):
     """ Returns a new child logger for the main spotpy application logging
 
     Use this logger to return new childs of the main logger"""
-    # Set the logger name, with the implementation class
-    return spotpy_logger.getChild(name)        
+
+    
+    return spotpy_logger.getChild(name)
