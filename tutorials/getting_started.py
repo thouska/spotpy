c2979062e3fc92772e239b6d2f70e204a7acbd5f
thouska <tobias.houska@umwelt.uni-giessen.de>
2022-09-08 11:35:40 +0200

    Fix black code style errors

--- tutorials/getting_started.py
+++ tutorials/getting_started.py
@@ -9,27 +9,31 @@ This class holds the example code from the getting_started web-documention.
 """
 # Getting started
 
-# To start your experience with SPOT you need to have SPOT installed. Please see the [Installation chapter](index.md) for further details.
-# To use SPOT we have to import it and use one of the pre-build examples:
-import spotpy  # Load the SPOT package into your working storage
-from spotpy.examples.spot_setup_rosenbrock import (
-    spot_setup,  # Import the two dimensional Rosenbrock example
-)
+# To start your experience with SPOTPY you need to have SPOTPY installed.
+# Please see the [Installation chapter](index.md) for further details.
+# To use SPOTPY we have to import it and use one of the pre-build examples:
+import spotpy  # Load the SPOTPY package into your working storage
+from spotpy.examples.spot_setup_rosenbrock import spot_setup
 
-# The example comes along with parameter boundaries, the Rosenbrock function, the optimal value of the function and RMSE as a likelihood.
-# So we can directly start to analyse the Rosenbrock function with one of the algorithms. We start with a simple Monte Carlo sampling:
+# The example comes along with parameter boundaries, the Rosenbrock function,
+# the optimal value of the function and RMSE as a likelihood.
+# So we can directly start to analyse the Rosenbrock function with one of the
+# algorithms. We start with a simple Monte Carlo sampling
+
 if __name__ == "__main__":
-    # Give Monte Carlo algorithm the example setup and saves results in a RosenMC.csv file
-    # spot_setup.slow = True
-    sampler = spotpy.algorithms.mc(spot_setup(), dbname="RosenMC", dbformat="ram")
+    # Give Monte Carlo algorithm the example setup and saves results in a
+    # RosenMC.csv file
+    sampler = spotpy.algorithms.mc(spot_setup(), dbname="RosenMC",
+                                   dbformat="ram")
 
     # Now we can sample with the implemented Monte Carlo algortihm:
     sampler.sample(50000)  # Sample 50.000 parameter combinations
     results = sampler.getdata()
-    # Now we want to have a look at the results. First we want to know, what the algorithm has done during the 10.000 iterations:
+    # Now we want to have a look at the results. First we want to know,
+    # what the algorithm has done during the 50.000 iterations:
     spotpy.analyser.plot_parameterInteraction(results)
+    # Now we collect the 10% runs with the lowest objective function
     posterior = spotpy.analyser.get_posterior(results, maximize=False)
     spotpy.analyser.plot_parameterInteraction(posterior)
     # Print the run with the lowest objective function
-    print(spotpy.analyser.get_best_parameterset(results))
-    
+    print(spotpy.analyser.get_best_parameterset(results, maximize=False))
