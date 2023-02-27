844abb73e927be9bc508c14cafe13f20f4b1b932
thouska <tobias.houska@umwelt.uni-giessen.de>
2023-02-27 17:01:13 +0100

    Remove unnecessary input

--- tutorials/tutorial_fast_hymod.py
+++ tutorials/tutorial_fast_hymod.py
@@ -9,9 +9,9 @@ This class holds example code how to use the FAST algorithm
 """
 
 import numpy as np
+
 import spotpy
 from spotpy.examples.spot_setup_hymod_python import spot_setup
-# from spotpy.examples.spot_setup_rosenbrock import spot_setup
 
 if __name__ == "__main__":
     parallel = "seq"
